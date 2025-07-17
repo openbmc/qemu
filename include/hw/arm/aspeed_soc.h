f610528b540c006ab9f1872fdb5526ee7da98071
Jamin Lin <jamin_lin@aspeedtech.com>
2025-07-17 11:40:30 +0800

    hw/arm/ast27x0: Move SSP coprocessor initialization from machine to SoC leve
    In the previous design, the SSP coprocessor (aspeed27x0ssp-soc) was initialized
    and realized at the machine level (e.g., AST2700FC). However, to make sure the
    coprocessors can work together properly—such as using the same SRAM, sharing
    the SCU, and having consistent memory remapping—we need to change how these
    devices are set up.
    
    This commit moves the SSP coprocessor initialization and realization into the
    AST2700 SoC (aspeed_soc_ast2700_init() and aspeed_soc_ast2700_realize()).
    By doing so, the SSP becomes a proper child of the SoC device, rather than
    the machine.
    
    This is a preparation step for future commits that will support shared SCU,
    SRAM, and memory remap logic—specifically enabling PSP DRAM remap for SSP SDRAM
    access.
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Link: https://lore.kernel.org/qemu-devel/20250717034054.1903991-3-jamin_lin@aspeedtech.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -128,6 +128,19 @@ struct Aspeed2600SoCState {
 #define TYPE_ASPEED2600_SOC "aspeed2600-soc"
 OBJECT_DECLARE_SIMPLE_TYPE(Aspeed2600SoCState, ASPEED2600_SOC)
 
+struct Aspeed27x0SSPSoCState {
+    AspeedSoCState parent;
+    AspeedINTCState intc[2];
+    UnimplementedDeviceState ipc[2];
+    UnimplementedDeviceState scuio;
+    MemoryRegion memory;
+
+    ARMv7MState armv7m;
+};
+
+#define TYPE_ASPEED27X0SSP_SOC "aspeed27x0ssp-soc"
+OBJECT_DECLARE_SIMPLE_TYPE(Aspeed27x0SSPSoCState, ASPEED27X0SSP_SOC)
+
 struct Aspeed27x0SoCState {
     AspeedSoCState parent;
 
@@ -135,6 +148,8 @@ struct Aspeed27x0SoCState {
     AspeedINTCState intc[2];
     GICv3State gic;
     MemoryRegion dram_empty;
+
+    Aspeed27x0SSPSoCState ssp;
 };
 
 #define TYPE_ASPEED27X0_SOC "aspeed27x0-soc"
@@ -145,18 +160,6 @@ struct Aspeed10x0SoCState {
 
     ARMv7MState armv7m;
 };
-
-struct Aspeed27x0SSPSoCState {
-    AspeedSoCState parent;
-    AspeedINTCState intc[2];
-    UnimplementedDeviceState ipc[2];
-    UnimplementedDeviceState scuio;
-
-    ARMv7MState armv7m;
-};
-
-#define TYPE_ASPEED27X0SSP_SOC "aspeed27x0ssp-soc"
-OBJECT_DECLARE_SIMPLE_TYPE(Aspeed27x0SSPSoCState, ASPEED27X0SSP_SOC)
 
 struct Aspeed27x0TSPSoCState {
     AspeedSoCState parent;
