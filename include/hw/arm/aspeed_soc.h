db7f070d91548ac9ad60ae359b98863ea266e3f6
Kane-Chen-AS <kane_chen@aspeedtech.com>
2025-12-08 15:44:15 +0800

    hw/misc: Add basic Aspeed PWM model
    Add an initial PWM model for Aspeed SoCs, including device state,
    register definitions, and basic initialization as a sysbus device.
    
    Signed-off-by: Kane-Chen-AS <kane_chen@aspeedtech.com>
    Link: https://lore.kernel.org/qemu-devel/20251208074436.1871180-4-kane_chen@aspeedtech.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -28,6 +28,7 @@
 #include "hw/misc/aspeed_hace.h"
 #include "hw/misc/aspeed_sbc.h"
 #include "hw/misc/aspeed_sli.h"
+#include "hw/misc/aspeed_pwm.h"
 #include "hw/watchdog/wdt_aspeed.h"
 #include "hw/net/ftgmac100.h"
 #include "target/arm/cpu.h"
@@ -108,7 +109,7 @@ struct AspeedSoCState {
     UnimplementedDeviceState video;
     UnimplementedDeviceState emmc_boot_controller;
     UnimplementedDeviceState dpmcu;
-    UnimplementedDeviceState pwm;
+    AspeedPWMState pwm;
     UnimplementedDeviceState espi;
     UnimplementedDeviceState udc;
     UnimplementedDeviceState ltpi;
